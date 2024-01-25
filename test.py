4854086604e096cdffb24c91ddf28378fb831c69
Clay Han <chan@clayhannf21rsos.lan>
2024-01-24 17:15:17 -0800

    test

---
+++ test.py
@@ -0,0 +1,20 @@
+import csv
+
+data = """Team,Arena,Address
+Atlanta Hawks,State Farm Arena,1 State Farm Drive, Atlanta, GA 30303
+Boston Celtics,TD Garden,100 Legends Way, Boston, MA 02114
+Brooklyn Nets,Barclays Center,620 Atlantic Ave, Brooklyn, NY 11217
+Charlotte Hornets,Spectrum Center,333 E Trade St, Charlotte, NC 28202
+Chicago Bulls,United Center,1901 W Madison St, Chicago, IL 60612
+Cleveland Cavaliers,Rocket Mortgage FieldHouse,1 Center Ct, Cleveland, OH 44115
+Dallas Mavericks,American Airlines Center,2500 Victory Ave, Dallas, TX 75219
+Denver Nuggets,Ball Arena,1000 Chopper Cir, Denver, CO 80204
+Detroit Pistons,Little Caesars Arena,2645 Woodward Ave, Detroit, MI 48201
+Golden State Warriors,Chase Center,1 Warriors Way, San Francisco, CA 94158"""
+
+# 1. Given the data above, turn it into a CSV that can be saved/opened locally
+
+# 2. After completing the above, add columns to the CSV that show the coordinates of the arena addresses
+
+# 3. Add a column to the csv that checks if any of the addresses are within 
+# 100 miles of 37.768149757128626, -122.38786647116393
